b3b4d8d31475925fa6d49326bfbde3f891540ec9
Marilyn Martha Yusnita Devi Parhusip <marilyn.parhusip@gmail.com>
2019-05-20 20:40:14 +0700

    First Push

--- testFireworks/GameScene.swift
+++ testFireworks/GameScene.swift
@@ -11,79 +11,126 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    
+    let label = SKLabelNode(text: "Happy Birthday")
+    let particle = SKEmitterNode(fileNamed: "MyParticle")!
+    let particleBlue = SKEmitterNode(fileNamed: "MyParticle")
+    let particleRed = SKEmitterNode(fileNamed: "MyParticle")
+
+    
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        addChild(label)
+        addChild(particle)
+        addChild(particleBlue!)
+        addChild(particleRed!)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        label.position = CGPoint(x: view.frame.width / 2 , y: view.frame.height / 2)
+        label.fontSize = 50
+        label.fontColor = SKColor.yellow
+        label.fontName = "Avenir"
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        
+        particle.position = CGPoint(x: CGFloat.random(in: 0...view.frame.width), y: CGFloat.random(in: 0...view.frame.height))
+        
+        // Fade Out Action
+//        let moveParticle = SKAction.fadeOut(withDuration: 1)
+//
+//        particle.run(moveParticle)
+        
+        
+        
+        particleBlue?.particleColor = UIColor.blue
+        particleBlue?.position = CGPoint(x: CGFloat.random(in: 0...view.frame.width), y: CGFloat.random(in: 0...view.frame.height))
+        
+        particleRed?.particleColor = UIColor.red
+        particleRed?.position = CGPoint(x: CGFloat.random(in: 0...view.frame.width), y: CGFloat.random(in: 0...view.frame.height))
+        
+        
+
+        
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(tap))
+        view.addGestureRecognizer(recognizer)
+        
+        
+        
+        
+        
+    }
+    
+    @objc func tap(recognizer: UITapGestureRecognizer) {
+//        let viewLocation = recognizer.location(in: view)
+//        let sceneLocation = convertPoint(fromView: viewLocation)
+//        let moveByAction = SKAction.moveBy(x: sceneLocation.x - label.position.x, y: sceneLocation.y - label.position.y, duration: 1)
+//       // label.run(moveByAction)
+//
+//        let moveByReverseAction = moveByAction.reversed()
+//        let moveByactions = [moveByAction, moveByReverseAction]
+//        let moveSequence = SKAction.sequence(moveByactions)
+//      //  let moveRepeatSequence = SKAction.repeat(moveSequence, count: 3)
+//        let moveForeverSequence = SKAction.repeatForever(moveSequence)
+//
+//        let scale = SKAction.scale(to: 0.5, duration: 1)
+//
+//        label.run(scale)
+        
+        
+        let viewLocation = recognizer.location(in: self.view)
+        let sceneLocation = convertPoint(fromView: viewLocation)
+        
+//        if atPoint(sceneLocation) == label {
+//            let scale = SKAction.scale(to: 0.5, duration: 1)
+//            label.run(scale)
+        
+//        if label.contains(sceneLocation) {
+//            let scale = SKAction.scale(to: 0.5, duration: 1)
+//            label.run(scale)
+        
+        
+        let sceneNodes = nodes(at: sceneLocation)
+        
+        for sceneNode in sceneNodes {
+//            if sceneNode == label {
+//                let scale = SKAction.scale(to: 0.5, duration: 1)
+//            let scale = SKAction.scaleX(to: 0.5, duration: 1)
+//                let scale = SKAction.scaleY(to: 2, duration: 1)
+//                let scale = SKAction.scaleX(to: 0.5, y: 2, duration: 1)
+//                label.run(scale)
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+            //let scale = SKAction.scale(by: 0.5, duration: 1)
+            
+//            let scale = SKAction.scaleX(by: 0.5, y: 2, duration: 1)
+//            let reverseScale = scale.reversed()
+//            let actions = [scale, reverseScale]
+//            let sequence = SKAction.sequence(actions)
+//            //let repeatSequence = SKAction.repeat(sequence, count: 5)
+//            let repeatSequence = SKAction.repeatForever(sequence)
+//            label.run(repeatSequence)
+//
+            
+            let move = SKAction.move(to: sceneLocation, duration: 1)
+//            let scale = SKAction.scaleX(to: 0.5, y: 2, duration: 1)
+            let scale = SKAction.scaleX(by: 0.5, y: 2, duration: 1)
+            let actions = [move, scale]
+            let sequence = SKAction.sequence(actions)
+            label.run(sequence)
+                break
+            }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
+//        let particleNodes = nodes(at: sceneLocation)
+//
+//        for sceneNode in sceneNodes {
+//            let moveParticle = SKAction.moveBy(x: CGFloat.random(in: 0...100), y: CGFloat.random(in: 0...100), duration: 1)
+//            let actionsParticle = [moveParticle]
+//            let sequenceParticle = SKAction.sequence(actionsParticle)
+//            particle.run(sequenceParticle)
+//            break
+        }
+        }
     
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
-}
+//    }
+
+
+
